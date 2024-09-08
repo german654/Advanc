708530a71ed7b3094b24c4e6f58b0a54fc6a57d2
vargas987 <rojhas123456@gmail.com>
2024-09-08 17:05:01 -0500

    avanxce subido

---
+++ app/src/main/java/pe/edu/upeu/asistenciaupeujcn/utils/TokenUtils.kt
@@ -0,0 +1,10 @@
+package pe.edu.upeu.asistenciaupeujcn.utils
+
+import android.content.Context
+
+object TokenUtils {
+    var TOKEN_CONTENT="Aqui va el Token"
+    var API_URL="http://localhost:8080/"
+    lateinit var CONTEXTO_APPX: Context
+    var USER_LOGIN=""
+}
